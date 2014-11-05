bac5cfd8e9abb2fa14668ffb25e42ac77de1939b
Travis <tmill3r@gmail.com>
2014-11-05 13:54:58 -0500

    new dataset of tweets included and TweetReader class to read in tweets

--- src/keyword/search/KeywordTester.java
+++ src/keyword/search/KeywordTester.java
@@ -1,9 +1,21 @@
 package keyword.search;
 
+import java.net.URL;
+import java.util.HashMap;
+
 public class KeywordTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> tweetDataBase = new HashMap<Integer, String>();
+		URL tweetsUrl = KeywordTester.class.getClassLoader().getResource("test_tweets.txt");
+		TweetReader TR = new TweetReader();
+		
+		tweetDataBase = TR.readTweets(tweetsUrl.getPath());
+		
+		System.out.println(tweetDataBase.get(20));
+		System.out.println(tweetDataBase.get(5000));
+		System.out.println(tweetDataBase.get(9998));
 
 	}
 
