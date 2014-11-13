330c9878b8094f15a2caed5e9caa7a1edb8c358e
Travis <tmill3r@gmail.com>
2014-11-12 23:48:32 -0500

    added createWordsIndex

--- src/keyword/search/KeywordTester.java
+++ src/keyword/search/KeywordTester.java
@@ -3,7 +3,9 @@ package keyword.search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class KeywordTester {
@@ -15,14 +17,49 @@ public class KeywordTester {
 		
 		System.out.println("Reading in Tweets...");
 		
-		tweetDataBase = TR.readTweets("res/test_tweets.txt");
+		tweetDataBase = TR.readTweets("res/tweets.txt");
 
-		System.out.println(tweetDataBase.get(1));
-		System.out.println(tweetDataBase.get(114));
-		System.out.println(tweetDataBase.get(9997));
-		//System.out.println(tweetDataBase.get(1231236));
-		//System.out.println(tweetDataBase.get(1600000));
 		
+		Map<String, List<Integer>> wordIndexDB = new HashMap<String, List<Integer>>();
+		TweetWordIndex TI = new TweetWordIndex();
+		List<Integer> tweetsList = new ArrayList<Integer>();
+		
+		System.out.println("Indexing Words...");
+		
+		wordIndexDB = TI.createIndex(tweetDataBase);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		try{ boolean done = false;
+			while (!done){
+				
+				System.out.println("Enter keyword or enter quit to stop");
+				String text = in.readLine();// read a base password
+				
+				if (text.equals("quit")) {
+					done = true;
+				} else {
+					
+					tweetsList = wordIndexDB.get(text);
+					
+					if (tweetsList != null) {
+						for (Integer tweet : tweetsList) {
+							System.out.println(tweet);
+							System.out.println(tweetDataBase.get(tweet));
+						}
+					} else {
+						System.out.println("Word not in the tweet database.");
+					}
+					
+				}
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("done");
 	}
 
 }
