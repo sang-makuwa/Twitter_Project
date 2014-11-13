9df29922586e6bdda8a610578c39d997e4468106
Travis <tmill3r@gmail.com>
2014-11-12 23:49:46 -0500

    adding TweetWordIndex again

---
+++ src/keyword/search/TweetWordIndex.java
@@ -0,0 +1,52 @@
+package keyword.search;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * TweetWordIndex indexes the words from tweets and creates a list
+ * of tweets containing that word.
+ * 
+ * @author travismiller
+ *
+ */
+public class TweetWordIndex {
+
+	/**
+	 * Creates an index of words from the tweets. Words are seperated from the 
+	 * tweet string by whitespace. All alphanumeric characters are removed and 
+	 * all the letters are made lowercase. The word is then placed in a hashmap
+	 * and all tweets containing the word are then added to an ArrayList.
+	 * 
+	 * @param tweetsMap a hashmap of all the tweets from the file
+	 * @return A hashmap with keys of all the words in the tweets and values of
+	 * ArrayLists of tweets which contain that word.
+	 */
+	Map<String, List<Integer>> createIndex(Map<Integer, String> tweetsMap) {
+		Map<String, List<Integer>> tweetWordIndex = new HashMap<String, List<Integer>>();
+
+		for (Map.Entry<Integer, String> tweetEntry : tweetsMap.entrySet()) {
+			
+			String[] words = tweetEntry.getValue().split("\\s+");
+			
+			for (String word: words){
+				word = word.replaceAll("[^a-zA-Z0-9]", "");
+				word = word.toLowerCase();
+				
+				List<Integer> tweetsList = tweetWordIndex.get(word);
+				
+				if (tweetsList == null) {
+					tweetsList = new ArrayList<Integer>();
+					tweetsList.add(tweetEntry.getKey());
+					tweetWordIndex.put(word, tweetsList);
+				} else {
+					tweetsList.add(tweetEntry.getKey());
+				}	
+			}
+		}
+		
+		return tweetWordIndex;
+	}
+}
